7a557e00a3a4254767af8c059f1ca51f1a6e667e
doge1024 <867129306@qq.com>
2020-04-15 11:27:41 +0800

    更新安装脚本

--- debug_install.sh
+++ debug_install.sh
@@ -1,3 +1,6 @@
+gem install parallel
+gem install cocoapods -v 1.8.4
+gem install cocoapods-generate -v 1.6.0
 gem uninstall cocoapods-bin
 gem uninstall cocoapods-bin-framework
 echo "\ngem build cocoapods-bin\n"
